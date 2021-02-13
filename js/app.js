f4862f692b8f4ba74e4aaf4d6c93246cf0ef995a
newnag <48855584+newnag@users.noreply.github.com>
2021-02-13 13:11:05 +0700

    12/02/2021

---
+++ js/app.js
@@ -0,0 +1,17 @@
+function open_acc_menu(){
+  document.querySelector('.acc-menu').classList.toggle('active')
+}
+
+function open_menu(){
+  document.querySelector('header nav').classList.toggle('active')
+  document.querySelector('header .hamburger').classList.toggle('active')
+}
+
+window.onscroll = function(e){
+  if(window.pageYOffset > 0){
+    document.querySelector('header').classList.add('scroll')
+  }
+  else{
+    document.querySelector('header').classList.remove('scroll')
+  }
+}
---
+++ js/home.js
@@ -0,0 +1,10 @@
+// slide home
+var swiper = new Swiper('.slide-head', {
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
